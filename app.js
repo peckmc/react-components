fde81b2018b2987fda72bd6c6d8b458c923cbaa6
mcpeck <peckmelodie@gmail.com>
2022-10-18 14:49:32 -0500

    Complete app.js for React Components sprint

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const style = {
+    fontWeight: isHovered ? 'bold' : 'normal'
+  };
+  return (
+    <li style={style} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      {props.item}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={['Bananas', 'Peanut butter', 'Bread']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
